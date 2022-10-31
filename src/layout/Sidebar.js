c7fe62ee94c9f147d6fdb987d79fa46b020d241d
sarperbg <sarpergulfirat@gmail.com>
2022-10-31 15:42:58 +0300

    icons add

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { ExploreIcon, HomeIcon, ListsIcon, MessagesIcon, MoreIcon, NotificationsIcon, PopulerIcon, ProfileIcon } from '../icons/Icon'
 
 const Sidebar = () => {
     return (
         <div className='w-72 bg-blue-200'>
             <h1>Sidebar</h1>
-
+            <HomeIcon/>
+            <ExploreIcon/>
+            <NotificationsIcon/>
+            <MessagesIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
         </div>
     )
 }
