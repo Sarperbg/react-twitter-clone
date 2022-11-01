a6c925ebb96dbf56e687e8d71b08501cb6286415
sarperbg <sarpergulfirat@gmail.com>
2022-11-01 13:53:16 +0300

    sidebar, logo , navigation completed

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,17 +1,62 @@
 import React from 'react'
+import SideLink from '../components/SideLink';
 import { ExploreIcon, HomeIcon, ListsIcon, MessagesIcon, MoreIcon, NotificationsIcon, PopulerIcon, ProfileIcon } from '../icons/Icon'
+import twitterLogo from "../images/twitter.svg"
 
+const sideLinks = [
+    {
+        name: "Home",
+        icon: HomeIcon,
+    },
+    {
+        name: "Explore",
+        icon: ExploreIcon,
+    },
+    {
+        name: "Notifications",
+        icon: NotificationsIcon,
+    },
+    {
+        name: "Messages",
+        icon: MessagesIcon,
+    },
+   
+    {
+        name: "Lists",
+        icon: ListsIcon,
+    },
+    {
+        name: "Profile",
+        icon: ProfileIcon,
+    },
+    {
+        name: "More",
+        icon: MoreIcon,
+    },
+];
 const Sidebar = () => {
     return (
-        <div className='w-72 bg-blue-200'>
-            <h1>Sidebar</h1>
-            <HomeIcon/>
-            <ExploreIcon/>
-            <NotificationsIcon/>
-            <MessagesIcon/>
-            <ListsIcon/>
-            <ProfileIcon/>
-            <MoreIcon/>
+        <div className='flex flex-col justify-between w-72 px-2'>
+            <div>
+                <div className='flex items-center justify-center w-12 h-12 rounded-full mt-1 mb-4 ml-1 hover:bg-gray-lightest'>
+                    <img src={twitterLogo} alt="Twitter logo" className="w-9 h-9" />
+
+                </div>
+                <nav className='mb-4'>
+                    <ul>
+                        {sideLinks.map(({ name, icon }) => (
+                            <SideLink
+                                key={name}
+                                name={name}
+                                Icon={icon}
+                            
+                            />
+                        ))}
+                    </ul>
+                </nav>
+                <button className='bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200'>Tweet</button>
+            </div>
+            <div>Alt Taraf</div>
         </div>
     )
 }
